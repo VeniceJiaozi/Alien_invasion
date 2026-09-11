d55b9ae7ec15f86be2cfe2680c89579eb98081cd
VeniceJiaozi <2366759931@qq.com>
2026-09-11 20:03:13 +0800

    增加了外星人“ ”

---
+++ alien.py
@@ -0,0 +1,29 @@
+import pygame
+from pygame.sprite import Sprite
+import os
+
+class Alien(Sprite):
+    '''表示单个外星人的类'''
+
+    def __init__(self,ai_settings,screen):
+        '''初始化外星人并设置初始位置'''
+        super(Alien,self).__init__()
+        self.screen=screen
+        self.ai_settings=ai_settings
+        current_dir=os.path.dirname(os.path.abspath(__file__))
+        image_path=os.path.join(current_dir,'images','alien.bmp')
+
+        #加载外星人图像并设置其rect属性
+        self.image=pygame.image.load(image_path)
+        self.rect=self.image.get_rect()
+
+        #每个外星人最初都在屏幕左上角附近
+        self.rect.x=self.rect.width
+        self.rect.y=self.rect.height
+
+        #存储外星人的准确位置
+        self.x=float(self.rect.x)
+
+    def blitme(self):
+        '''在指定位置绘制外星人'''
+        self.screen.blit(self.image,self.rect)
